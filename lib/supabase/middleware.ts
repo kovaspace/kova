7e9870f614bc4f7c96720abe28cd7fb9b7b1bd4f
Anthony Reyes <anthonyreyesf@gmail.com>
2024-11-17 16:20:34 -0500

    redirect to app if user is logged in and trying to access root url

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -46,6 +46,11 @@ export async function updateSession(request: NextRequest) {
     const url = request.nextUrl.clone();
     url.pathname = "/";
     return NextResponse.redirect(url);
+  } else if (user && !isProtectedRoute) {
+    // user is logged in but trying to access a public route, redirect to dashboard
+    const url = request.nextUrl.clone();
+    url.pathname = "/dashboard";
+    return NextResponse.redirect(url);
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
